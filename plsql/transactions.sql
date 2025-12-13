459f958b3100dfcada49324025ad49ecfcabc91a
Charles Fiorito <charles.fiorito@gmail.com>
2025-12-13 14:39:00 -0500

    NEW(vue_securite): Gestion des privilèges

---
+++ plsql/transactions.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE PROCEDURE planifier_experience (
+    p_id_projet     IN NUMBER,
+    p_titre_exp     IN VARCHAR2,
+    p_date_real     IN DATE,
+    p_id_equipement IN NUMBER
+) IS
+    v_id_exp EXPERIENCE.id_exp%TYPE;
+BEGIN
+    INSERT INTO EXPERIENCE (
+        id_exp,
+        id_projet,
+        titre_exp,
+        date_realisation,
+        statut
+    )
+    VALUES (
+        seq_experience.NEXTVAL,
+        p_id_projet,
+        p_titre_exp,
+        p_date_real,
+        'En cours'
+    )
+    RETURNING id_exp INTO v_id_exp;
+
+    SAVEPOINT avant_affectation_equipement;
+
+    INSERT INTO AFFECTATION_EQUIP (
+        id_affect,
+        id_projet,
+        id_equipement,
+        date_affectation
+    )
+    VALUES (
+        seq_affect.NEXTVAL,
+        p_id_projet,
+        p_id_equipement,
+        SYSDATE
+    );
+
+    COMMIT;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK TO avant_affectation_equipement;
+        COMMIT;
+END;
+/
