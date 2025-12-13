cdd182494697e52e809d25c796741c519e1edad3
Hasan Al-dulaimi <haald001@edu.cegepgarneau.ca>
2025-12-12 22:19:45 -0500

    procedure fait

--- sql/creation_utilisateur.sql
+++ sql/creation_utilisateur.sql
@@ -1,41 +1,42 @@
 -- Creation des utilisateurs
 --Adminsitrateur
-create user admin_lab identified by AdminLab#2025
+create user admin_lab identified by adminlab#2025
    default tablespace users
    temporary tablespace temp
    quota unlimited on users;
 
 --Gestionnaire de laboratoire
-create user gest_lab identified by GestLab#2025
+create user gest_lab identified by gestlab#2025
    default tablespace users
    temporary tablespace temp
    quota 100M on users;
 
 --lecteur
-create user lect_lab identified by LectLab#2025
+create user lect_lab identified by lectlab#2025
    default tablespace users
    temporary tablespace temp
    quota 50M on users;
 
 --Creation des roles
-CREATE ROLE role_lab_admin;
-CREATE ROLE role_lab_gest;
-CREATE ROLE role_lab_lect;
-CREATE ROLE role_lab_research;
+create role role_lab_admin;
+create role role_lab_gest;
+create role role_lab_lect;
+create role role_lab_research;
 
 
 -- Droits de connexion
-GRANT CREATE SESSION TO role_lab_research;
+grant create session to role_lab_research;
 
 --Droits Admin
-GRANT CREATE TABLE, CREATE VIEW, CREATE SEQUENCE,
-CREATE PROCEDURE, CREATE FUNCTION, CREATE TRIGGER
-TO role_lab_admin;
+grant create table,
+   create view,
+   create sequence,
+   create procedure,
+   create trigger
+to role_lab_admin;
 
 
 --Assignation des roles
-GRANT role_lab_admin, role_lab_research TO admin_lab;
-GRANT role_lab_gest, role_lab_research TO gest_lab;
-GRANT role_lab_lect, role_lab_research TO lect_lab;
-
-
+grant role_lab_admin,role_lab_research to admin_lab;
+grant role_lab_gest,role_lab_research to gest_lab;
+grant role_lab_lect,role_lab_research to lect_lab;
