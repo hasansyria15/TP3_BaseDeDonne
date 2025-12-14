f3dac6f98ea59096c7954a539a51099994940789
FiorC <charles.fiorito@gmail.com>
2025-12-13 23:31:36 -0500

    Update transactions.sql

--- plsql/transactions.sql
+++ plsql/transactions.sql
@@ -1,47 +1,44 @@
-CREATE OR REPLACE PROCEDURE planifier_experience (
-    p_id_projet     NUMBER,
-    p_titre_exp     VARCHAR2,
-    p_date_real     DATE,
-    p_id_equipement NUMBER
-) IS
-    v_id_exp EXPERIENCE.id_exp%TYPE;
-BEGIN
-    INSERT INTO EXPERIENCE (
-        id_exp,
-        id_projet,
-        titre_exp,
-        date_realisation,
-        statut
-    )
-    VALUES (
-        seq_experience.NEXTVAL,
-        p_id_projet,
-        p_titre_exp,
-        p_date_real,
-        'En cours'
-    )
-    RETURNING id_exp INTO v_id_exp;
+-- Procédure : PLANIFIER_EXPERIENCE
+create or replace procedure planifier_experience (
+ p_id_projet number,
+ p_titre_exp varchar2,
+ p_date_real date,
+ p_id_equipement number
+) is
+ v_id_exp experience.id_exp%type;
+begin
+ insert into experience (
+  id_exp,
+  id_projet,
+  titre_exp,
+  date_realisation,
+  statut
+ ) values (
+  seq_experience.nextval,
+  p_id_projet,
+  p_titre_exp,
+  p_date_real,
+  'En cours'
+ ) returning id_exp into v_id_exp;
 
-    SAVEPOINT avant_affectation_equipement;
+ savepoint avant_affectation_equipement;
 
-    INSERT INTO AFFECTATION_EQUIP (
-        id_affect,
-        id_projet,
-        id_equipement,
-        date_affectation
-    )
-    VALUES (
-        seq_affect.NEXTVAL,
-        p_id_projet,
-        p_id_equipement,
-        SYSDATE
-    );
+ insert into affectation_equip (
+  id_affect,
+  id_projet,
+  id_equipement,
+  date_affectation
+ ) values (
+  seq_affect.nextval,
+  p_id_projet,
+  p_id_equipement,
+  sysdate
+ );
 
-    COMMIT;
-
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK TO avant_affectation_equipement;
-        COMMIT;
-END;
+ commit;
+exception
+ when others then
+  rollback to avant_affectation_equipement;
+  commit;
+end planifier_experience;
 /
